5b80db5bb171f4beeec39872e25d174b95505a54
HJoonKwon <dsazz801@gmail.com>
2022-08-20 17:12:33 +0900

    skeleton competition metric

--- evaluate.py
+++ evaluate.py
@@ -3,4 +3,10 @@ import torch
 
 @torch.inference_mode()
 def eval_one_epoch():
+    """ compute loss and prediction score during training"""
+    pass
+
+@torch.inference_mode()
+def compute_metric():
+    """ compute competition metric"""
     pass
