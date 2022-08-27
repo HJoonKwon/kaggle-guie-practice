1d31dca4fa4a5ed81f1349d127298cd7d8106af5
HJoonKwon <dsazz801@gmail.com>
2022-08-27 14:58:54 +0900

    feat: setup funcs for ddp(from detr)

--- utils.py
+++ utils.py
@@ -6,33 +6,29 @@ https://github.com/facebookresearch/detr/blob/master/util/misc.py
 import torch.distributed as dist
 import torch
 
-def is_dist_avail_and_initialized():
-    if not dist.is_available():
-        return False
-    if not dist.is_initialized():
-        return False
-    return True
 
+def init_for_distributed(rank, opts):
 
-def get_world_size():
-    if not is_dist_avail_and_initialized():
-        return 1
-    return dist.get_world_size()
+    # 1. setting for distributed training
+    opts.rank = rank
+    opts.num_workers = len(opts.gpu_ids) * 4
+    opts.world_size = len(opts.gpu_ids)
+    local_gpu_id = int(opts.gpu_ids[opts.rank])
+    torch.cuda.set_device(local_gpu_id)
+    if opts.rank is not None:
+        print("Use GPU: {} for training".format(local_gpu_id))
 
+    # 2. init_process_group
+    dist.init_process_group(backend='nccl',
+                            init_method='tcp://127.0.0.1:23456',
+                            world_size=opts.world_size,
+                            rank=opts.rank)
 
-def get_rank():
-    if not is_dist_avail_and_initialized():
-        return 0
-    return dist.get_rank()
-
-
-def is_main_process():
-    return get_rank() == 0
-
-
-def save_on_master(*args, **kwargs):
-    if is_main_process():
-        torch.save(*args, **kwargs)
+    # if put this function, the all processes block at all.
+    dist.barrier()
+    # convert print fn iif rank is zero
+    setup_for_distributed(opts.rank == 0)
+    print(opts)
 
 
 def setup_for_distributed(is_master):
@@ -47,4 +43,4 @@ def setup_for_distributed(is_master):
         if is_master or force:
             builtin_print(*args, **kwargs)
 
-    __builtin__.print =
+    __builtin__.print = print
