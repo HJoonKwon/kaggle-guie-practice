3a0a4b66bd2a493b109865e494e13f5bd13f6066
HJoonKwon <dsazz801@gmail.com>
2022-08-28 21:17:12 +0900

    feat: update todo list

--- train.py
+++ train.py
@@ -27,8 +27,9 @@ from preprocess import preprocess_main
 from utils import init_for_distributed, setup_for_distributed
 
 ##TODO:: different model output for training/inference (https://www.kaggle.com/code/motono0223/guie-clip-tensorflow-train-example/notebook)
-##TODO:: syncnorm?
-##TODO:: best model?
+##TODO:: add syncbatchnorm at linear projection?
+##TODO:: best model? (selected metric by user)
+##TODO:: train_one_epoch/ evaluate function
 
 
 def setup_run(args: ConfigType):
